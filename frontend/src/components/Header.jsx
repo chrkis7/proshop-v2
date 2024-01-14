06bfe37684dda7bb5b92cf8d56b5862c48f7cc75
Chris Kim <chriskim@chriss-mbp.home>
2024-01-13 21:59:19 -0500

    Header & footer components

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,28 @@
+import { Navbar, Nav, Container } from 'react-bootstrap'
+import { FaCartShopping, FaUser } from 'react-icons/fa6' 
+
+
+const Header = () => {
+  return (
+    <header>
+      <Navbar bg='dark' data-bs-theme='dark' expand='md' collapseOnSelect>
+        <Container>
+          <Navbar.Brand href='/'>ProShop</Navbar.Brand>
+          <Navbar.Toggle aria-controls='basic-navbar-nav' />
+          <Navbar.Collapse id='basic-navbar-nav'>
+            <Nav className='ms-auto'>
+              <Nav.Link href='/cart'>
+                <FaCartShopping /> Cart
+              </Nav.Link>
+              <Nav.Link href='/login'>
+                <FaUser /> Sign In
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  )
+}
+
+export default Header

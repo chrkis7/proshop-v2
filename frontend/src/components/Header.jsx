32763e245d55780e24a3c1bb0f5760c4b065b63d
Chris Kim <chriskim@chriss-mbp.home>
2024-01-14 02:58:19 -0500

    Set up react router

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,5 +1,6 @@
 import { Navbar, Nav, Container } from 'react-bootstrap'
 import { FaCartShopping, FaUser } from 'react-icons/fa6'
+import { LinkContainer } from 'react-router-bootstrap'
 import logo from '../assets/logo.png'
 
 
@@ -8,18 +9,24 @@ const Header = () => {
     <header>
       <Navbar bg='dark' data-bs-theme='dark' expand='md' collapseOnSelect>
         <Container>
-          <Navbar.Brand href='/'>
-            <img src={logo} alt='Logo' />
-            ProShop</Navbar.Brand>
+          <LinkContainer to='/'>
+            <Navbar.Brand>
+              <img src={logo} alt='Logo' />
+              ProShop</Navbar.Brand>
+          </LinkContainer>
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav'>
             <Nav className='ms-auto'>
-              <Nav.Link href='/cart'>
-                <FaCartShopping /> Cart
-              </Nav.Link>
-              <Nav.Link href='/login'>
-                <FaUser /> Sign In
-              </Nav.Link>
+              <LinkContainer to='/cart'>
+                <Nav.Link>
+                  <FaCartShopping /> Cart
+                </Nav.Link>
+              </LinkContainer>
+              <LinkContainer to='/login'>
+                <Nav.Link>
+                  <FaUser /> Sign In
+                </Nav.Link>
+              </LinkContainer>
             </Nav>
           </Navbar.Collapse>
         </Container>

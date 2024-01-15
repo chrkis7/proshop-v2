6821aa2f3129e2bfbc0e284e6dca2c21b937a9e1
Chris Kim <chriskim@chriss-mbp.home>
2024-01-15 16:40:46 -0500

    Database connect & model data

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,13 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import colors from 'colors'
 dotenv.config()
+import connectDB from './config/db.js'
 import products from './data/products.js'
 
 const port = process.env.PORT | 2000
+
+connectDB() // Connect to MongoDB
 
 const app = express()
 
@@ -11,13 +15,13 @@ app.get('/', (req, res) => {
   res.send('API is running...')
 })
 
-app.get('/products', (req, res) => {
+app.get('/api/products', (req, res) => {
   res.json(products)
 })
 
-app.get('/product/:id', (req, res) => {
+app.get('/api/products/:id', (req, res) => {
   const product = products.find((product) => product._id === req.params.id)
   res.json(product)
 })
 
-app.listen(port, () => console.log(`Server is running on port ${port}`))
+app.listen(port, () => console.log(`Server is running on port ${port}`.cyan))

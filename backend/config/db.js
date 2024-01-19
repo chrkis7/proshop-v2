d122e19e9ec0d94bfae1a675c67dd28092e24aa1
Chris Kim <chriskim@chriss-mbp.home>
2024-01-19 00:04:22 -0500

    Data seeder & error middleware & product controller

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,10 +3,10 @@ import mongoose from 'mongoose'
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI)
-    console.log(`MongoDB connected: ${conn.connection.host}`.magenta)
+    console.log(`MongoDB connected: ${conn.connection.host}`.cyan.inverse)
     
   } catch (err) {
-    console.log(`Error: ${err.message}`.red)
+    console.log(`Error: ${err.message}`.red.inverse)
     process.exit(1)
   }
 }
--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,8 @@ import dotenv from 'dotenv'
 import colors from 'colors'
 dotenv.config()
 import connectDB from './config/db.js'
-import products from './data/products.js'
+import {notFound, errorHandler} from './middleware/errorHandler.js'
+import productRoutes from './routes/productRoutes.js'
 
 const port = process.env.PORT | 2000
 
@@ -15,13 +16,9 @@ app.get('/', (req, res) => {
   res.send('API is running...')
 })
 
-app.get('/api/products', (req, res) => {
-  res.json(products)
-})
+app.use('/api/products', productRoutes)
 
-app.get('/api/products/:id', (req, res) => {
-  const product = products.find((product) => product._id === req.params.id)
-  res.json(product)
-})
+app.use(notFound)
+app.use(errorHandler)
 
 app.listen(port, () => console.log(`Server is running on port ${port}`.cyan))

1a75a11957cdb813410e476cf6d395ca21a30865
Chris Kim <chriskim@chriss-mbp.home>
2024-01-23 02:56:40 -0500

    User controller & user routes

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,12 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import cookieParser from 'cookie-parser'
 import colors from 'colors'
 dotenv.config()
 import connectDB from './config/db.js'
 import {notFound, errorHandler} from './middleware/errorHandler.js'
 import productRoutes from './routes/productRoutes.js'
+import userRoutes from './routes/userRoutes.js'
 
 const port = process.env.PORT | 2000
 
@@ -12,11 +14,17 @@ connectDB() // Connect to MongoDB
 
 const app = express()
 
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+app.use(cookieParser())
+
 app.get('/', (req, res) => {
   res.send('API is running...')
 })
 
 app.use('/api/products', productRoutes)
+app.use('/api/users', userRoutes)
 
 app.use(notFound)
 app.use(errorHandler)

7ca3b83091bdfdd5becff9e5c37e76e44768155e
Chris Kim <chriskim@chriss-mbp.home>
2024-01-30 23:31:00 -0500

    Enable creating review for product

--- frontend/src/slices/usersApiSlice.js
+++ frontend/src/slices/usersApiSlice.js
@@ -1,4 +1,4 @@
-import { USERS_URL } from '../constants'
+import { PRODUCTS_URL, USERS_URL } from '../constants'
 import { apiSlice } from './apiSlice'
 
 export const usersApiSlice = apiSlice.injectEndpoints({
@@ -36,8 +36,28 @@ export const usersApiSlice = apiSlice.injectEndpoints({
       }),
       providesTags: ['Users'],
       keepUnusedDataFor: 5
-    })
+    }),
+    deleteUser: builder.mutation({
+      query: (userId) => ({
+        url: `${USERS_URL}/${userId}`,
+        method: 'DELETE'
+      })
+    }),
+    getUserDetails: builder.query({
+      query: (userId) => ({
+        url: `${USERS_URL}/${userId}`
+      }),
+      keepUnusedDataFor: 5
+    }),
+    updateUser: builder.mutation({
+      query: ( data ) => ({
+        url: `${USERS_URL}/${data.userId}`,
+        method: 'PUT',
+        body: data,
+      }),
+      invalidatesTags: ['Users']
+    })  
   })
 })
 
-export const { useLoginMutation, useLogoutMutation, useRegisterMutation, useProfileMutation, useGetUsersQuery } = usersApiSlice
+export const { useLoginMutation, useLogoutMutation, useRegisterMutation, useProfileMutation, useGetUsersQuery, useDeleteUserMutation, useGetUserDetailsQuery, useUpdateUserMutation } = usersApiSlice
